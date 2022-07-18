4ac511db218514d68a6db820bbb3cf14fdfea459
zhinan.li <zhinan.li@cootek.com>
2022-07-18 15:28:27 +0800

    xlua引入 & VCode构建 & 更新gitignore

---
+++ Taro/Assets/CardPlay/Script/CardGameLauncher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+namespace Taro.CardPlay
+{
+    public class CardGameLauncher : MonoBehaviour
+    {
+        private LuaEnv luaEnv;
+
+        void Start()
+        {
+            luaEnv = new LuaEnv();
+            luaEnv.DoString("require 'Lua/CardPlay_Main'");
+        }
+
+        private void OnDestroy() 
+        {
+            luaEnv.Dispose();
+            luaEnv = null;
+        }
+    }
+}
+
