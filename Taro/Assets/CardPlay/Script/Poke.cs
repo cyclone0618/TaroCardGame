d54886c4763d848d89c2a18b4de49dc08b0511a7
zhinan.li <zhinan.li@cootek.com>
2022-07-19 18:11:27 +0800

    扑克初步

---
+++ Taro/Assets/CardPlay/Script/Poke.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Taro.CardPlay
+{
+    public class Poke : MonoBehaviour
+    {
+        private Text text;
+
+        public void SetContent(int figure, int pattern)
+        {
+            
+        }
+    }
+}
+
